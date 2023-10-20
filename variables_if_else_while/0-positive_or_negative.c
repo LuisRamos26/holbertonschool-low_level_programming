d11ea73ce5c5ee80e91f7231a9e9e83509bf45d8
LuisRamos26 <146479847+LuisRamos26@users.noreply.github.com>
2023-10-20 15:34:57 -0700

    Update 0-positive_or_negative.c

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,13 +1,7 @@
 #include <stdlib.h>
-
 #include <time.h>
-
 /* more headers goes there */
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
+
 /* betty style doc for function main goes there */
 int main(void)
 {
