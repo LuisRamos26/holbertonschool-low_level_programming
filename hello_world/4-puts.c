c5489b8614e20a2954f627fbf6c729cd8bbb384d
LuisRamos26 <146479847+LuisRamos26@users.noreply.github.com>
2023-10-19 22:24:33 -0700

    Update 4-puts.c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -3,7 +3,7 @@
 int main()
 
 {	
- char Text[] = /""Programming is like building a multilingual puzzle";
+ char Text[] = /*""Programming is like building a multilingual puzzle";
  puts(Text); 
  return 0;
  }
