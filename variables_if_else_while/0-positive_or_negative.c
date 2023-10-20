dcfe34357ad1af7a9f3833210a4b96ff9d8d9d24
LuisRamos26 <146479847+LuisRamos26@users.noreply.github.com>
2023-10-20 15:16:32 -0700

    Update 0-positive_or_negative.c

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -12,8 +12,10 @@
 int main(void)
 {
 	int n;
+	
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
+	
 	/* your code goes there */
 	if (n>0)
 		{
@@ -27,5 +29,6 @@ int main(void)
        		{                                                                                                  
 		printf("%d is negative\n", n);                                          
 	      	}     
+
 	return (0);
 }
