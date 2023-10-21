d0f1c677746babf143dbc408b3d8dc7d247f68fc
LuisRamos26 <angelramos261098@gmail.com>
2023-10-21 10:03:12 -0700

    base 16

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/* more headers goes there */
+
+/**
+* main - Entry point
+* Description: 'How to print numbers base 10'
+* Return: Always 0 (Success)
+*/
+/* betty style doc for function main goes there */
+
+int main(void)
+{
+	int number;
+	char letter;
+/* print numbers */
+	for (number = '0'; number <= '9'; number++)
+		putchar(number);
+	for (letter = 'a'; letter <= 'f'; letter++)
+	      putchar(letter)	
+	putchar('\n');
+	return (0);
+}
