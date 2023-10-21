35e732687c5a0293b6f1975faabeec1158317a51
LuisRamos26 <angelramos261098@gmail.com>
2023-10-21 09:41:14 -0700

    q and w

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -1,10 +1,10 @@
-#'include <stdio.h>
+#include <stdio.h>
 
 /* more headers goes there */
 
 /**
 * main - Entry point
-* Description: 'How to print alphabet'
+* Description: 'How to print alphabet whitout q and e'
 * Return: Always 0 (Success)
 */
 /* betty style doc for function main goes there */
