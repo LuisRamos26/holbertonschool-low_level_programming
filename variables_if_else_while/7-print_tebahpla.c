ed83168c5d4429a6e6d77e8588218024def49bcb
LuisRamos26 <angelramos261098@gmail.com>
2023-10-21 09:55:37 -0700

    alphabet

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/* more headers goes there */
+
+/**
+* main - Entry point
+* Description: 'How to print alphabet'
+* Return: Always 0 (Success)
+*/
+/* betty style doc for function main goes there */
+
+int main(void)
+{
+	char alphabet;
+/* print alphabet*/
+	for (alphabet = 'z'; alphabet <= 'a'; alphabet--)
+		putchar (alphabet);
+	putchar ('\n');
+	return (0);
+}
