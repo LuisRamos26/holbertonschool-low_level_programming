4db68a2416c214898f9183e8f4ba9383d20dfa7a
LuisRamos26 <angelramos261098@gmail.com>
2023-10-21 09:51:09 -0700

    char numbers

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/* more headers goes there */
+
+/**
+* main - Entry point
+* Description: 'How to print numbers base 10'
+* Return: Always 0 (Success)
+*/
+/* betty style doc for function main goes there */
+
+int main(void)
+{
+	char number;
+/* print numbers */
+	for (number = 0; number <= 9; number++)
+		putchar (number);
+	putchar('\n');
+	return (0);
+}
