809268bb5d311134ca16f3e638a842413fcee436
LuisRamos26 <146479847+LuisRamos26@users.noreply.github.com>
2023-10-21 09:24:44 -0700

    Update 2-print_alphabet.c

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -14,7 +14,9 @@ int main(void)
 	char alphabet;
 /* print alphabet*/
 	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
-		putchar (alphabet);
+		{
+			putchar (alphabet);
+		}
 	putchar ('\n');
 	return 0;
 }
