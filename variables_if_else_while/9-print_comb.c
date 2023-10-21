18ba8f8e367d13fbc1c4527609de51816d77affd
LuisRamos26 <angelramos261098@gmail.com>
2023-10-21 10:21:37 -0700

    numbers

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/* more headers goes there */
+
+/**
+* main - Entry point
+* Description: 'How to print numbers base 10'
+* Return: Always 0 (Success)
+*/
+/* betty style doc for function main goes there */
+
+int main(void)
+{
+	int number;
+/* print numbers */
+	for (number = 0; number <= 9; number++)
+	{
+		putchar("%d", number);
+		if (number < 9)
+			putchar(", ");
+		else
+			putchar (number < 9);
+
+	}
+	printf("\n");
+	return (0);
+}
