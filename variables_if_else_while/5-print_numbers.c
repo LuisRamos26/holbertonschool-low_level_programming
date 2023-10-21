343a42795f8842358c5ef1fa67f77970cd41bc21
LuisRamos26 <angelramos261098@gmail.com>
2023-10-21 09:46:15 -0700

    print numbers

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/* more headers goes there */
+
+/**
+* main - Entry point
+* Description: 'How to print numbers base 10'
+* Return: Always 0 (Success)
+*/
+/* betty style doc for function main goes there */
+
+int main(void)
+{
+	int number;
+/* print numbers*/
+	for (number = 0; number <= 9; number++)
+		printf ("%d", number);
+	printf ('\n');
+	return (0);
+}
