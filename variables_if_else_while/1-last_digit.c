918e6df7685d98503aa43f12046d8a54e8d277df
LuisRamos26 <146479847+LuisRamos26@users.noreply.github.com>
2023-10-21 08:37:24 -0700

    Update 1-last_digit.c

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -1,7 +1,6 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-#include <string.h>
 
 /* more headers goes there */
 /**
@@ -13,18 +12,16 @@
 int main(void)
 {
 	int n;
-	int last;
 	/* identify numbers */
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
 	printf( "Last digit of %d is ", n);
-	last = strlen (n);
-	if (last > 5)
-		printf("%d and is greater than 5\n", last);
-	else if (n == 0)
-		printf("%d and is 0\n", last);
+	if (n % 10 > 5)
+		printf("%d and is greater than 5\n", n % 10);
+	else if (n % 10 == 0)
+		printf("%d and is 0\n", n % 10);
 	else
-		printf("%d and is less than 6 and not 0\n", last);
+		printf("%d and is less than 6 and not 0\n", n % 10);
 	return (0);
 }
