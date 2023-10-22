f353133d23c63bc47c63c62cce169ec9d2424a63
LuisRamos26 <angelramos261098@gmail.com>
2023-10-22 12:43:04 -0700

    sing

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * _isalpha - Check main.h
+ * @c: is an input character
+ * Description: function uses to identify lowercase and uppercase characters
+ * Return: 1 if is lowercase/uppercar
+ */
+
+int _isalpha(int c)
+{
+	int value;
+	int result;
+	/* function*/
+	if (c > 0)
+	{
+		value = 1;
+		result = _putchar('+, %d', value);
+	}
+	else if (c == 0)
+	{
+		value = 0;
+		result = _putchar('0, %d', value);
+	}
+	else
+	{
+		value = -1;
+		result = _putchar('-, %d', value);
+	}
+
+	return (result);
+}
