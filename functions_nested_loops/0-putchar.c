ad6853acc4d046ef2ba9832ff0fa37df62eb81b7
LuisRamos26 <146479847+LuisRamos26@users.noreply.github.com>
2023-10-22 11:03:20 -0700

    Update 0-putchar.c

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -9,9 +9,8 @@ int main(void)
 	char text[] = "_putchar";
 	int i;
 
-	for (i = 0, i < 8, i++)
+	for (i = 0; i < 8; i++)
 		_putchar(text[i]);
-	for (i = 0, i < 8, i++)
-		_putchar(text[i]);
+	_putchar('\n');
 	return (0);
 }
