9d10ba518bedc830cdeba84610b694a751487b8b
LuisRamos26 <angelramos261098@gmail.com>
2023-10-22 12:26:28 -0700

    isalpha

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _islower - Check main.h
+ * @c: is an input character
+ * Description: function uses to identify lowercase characters
+ * Return: 1 if is lowercase
+ */
+
+int _isalpha(int c)
+{
+	char i;
+	char j;
+	int result = 0;
+	/* function*/
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		for (j = 'A'; j <= 'Z', j++)
+		{
+			if (i == c || j ==  c)
+				result = 1;
+		}
+	return (result);
+}
