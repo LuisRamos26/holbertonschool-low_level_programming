b6bd5662e0e75538a546ab3d2162f1d725035b5e
LuisRamos26 <146479847+LuisRamos26@users.noreply.github.com>
2023-10-22 11:21:24 -0700

    Update 1-alphabet.c

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -6,13 +6,12 @@
 *Return: Alwavys 0 (Succes)
 */
 
-int main(void)
+void print_alphabet(void)
 {
-	char alphabet[] = "abcdefghijklmnopqrstuvwxyz";
-	int i;
-		
-	for (i = 0; i < 26; i++)
-		print_alphabet(alphabet[i]);
+	char i;
+	
+	for (i = 'a'; i <= 'z'; i++)
+		_putchar(i);
 	_putchar('\n');
 	return (0);
 }
