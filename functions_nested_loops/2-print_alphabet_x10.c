44c2f025227943f80decd1115520815c3bfbfdcc
LuisRamos26 <146479847+LuisRamos26@users.noreply.github.com>
2023-10-22 11:50:20 -0700

    Update 2-print_alphabet_x10.c

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -11,10 +11,11 @@ void print_alphabet_x10(void)
 	char j;
 	/* function*/
 	for (i = 1; i <= 10; i++)
+		{
 		for (j = 'a'; j <= 'z'; j++)
-			{
-				_putchar(j);
-				_putchar('\n');	
-			}
+			_putchar(j);
+		_putchar('\n');		
+		}
+		
 			
 }
