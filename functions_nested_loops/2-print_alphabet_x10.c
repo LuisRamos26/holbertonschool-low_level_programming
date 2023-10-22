a802df46427a4437d52bf90c27690621f9c6effd
LuisRamos26 <angelramos261098@gmail.com>
2023-10-22 11:44:13 -0700

    print alphabet 10 times

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -3,8 +3,7 @@
 /**
  * print_alphabet_x10 - Check description
  * Description: prints the alphabet in lowercase 10 times
- * Return: Nothing.
- */
+ * Return: Nothing.*/
 
 void print_alphabet_x10(void);
 {
