fa90f145805c75a7c23fbdd4ab394ff2c019cd9e
LuisRamos26 <angelramos261098@gmail.com>
2023-10-22 12:56:21 -0700

    abs

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_sign - Check main.h
+ * @n: is an input number
+ * Description: function uses to identify sing of the number
+ * Return: 1 for positive numbers, 0 for zero and -1 for negative
+ */
+
+int _abs(int)
+{
+	int value;
+	/*function]*/
+	if (value > 0)
+		return (value);
+	else
+	{
+		value = value * -1;
+		return (value);
+	}
+}
