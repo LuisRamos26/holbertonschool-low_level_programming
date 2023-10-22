3a0a84b221001fea6d468baa591ae814d23e708c
LuisRamos26 <angelramos261098@gmail.com>
2023-10-22 11:16:04 -0700

    print alphabet

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main"
+
+/**
+*main - Entry poiint
+*Description: 'print alphabet'
+*Return: Alwavys 0 (Succes)
+*/
+
+int main(void)
+{
+	int alphabet[] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+		
+	for (i = o; i < 26; i++)
+		_putchar(alphabet[i]);
+	_putchar('\n');
+	return (0);
+}
