b8a79d0b3961160c96d525d69ad98df8a03eb155
LuisRamos26 <angelramos261098@gmail.com>
2023-10-22 13:30:36 -0700

    last digit

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_last_digit - Check main
+ * @r: An integer input
+ * Description: print last digit
+ * Return: Last digit
+ */
+int print_last_digit(int value)
+{
+	
+	return (result[value])
+	
+}
