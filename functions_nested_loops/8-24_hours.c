2e2c015b145b78a4a616f50450d5a8ed1a7655a5
LuisRamos26 <angelramos261098@gmail.com>
2023-10-22 19:32:22 -0700

    24

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * jack_bauer - Check Holberton
+ * @r: An integer input
+ * Description: This function prints the last digit of a number
+ * Return: last digit of number r
+ */
+void jack_bauer(void);
+{
+	int i;
+	int j;
+	/*function*/
+	for (i = 0; i <=24; i++)
+	{
+		for (j = 0; j <=60; j++)
+			_putchar
+	}
+
+	_putchar((n % 10) + '0');
+	return (n % 10);
+}
