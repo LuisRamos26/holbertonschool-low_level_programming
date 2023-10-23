7c9fe116735bae528d2d6f265ad0dcca0c41c235
LuisRamos26 <146479847+LuisRamos26@users.noreply.github.com>
2023-10-22 19:47:24 -0700

    Update 9-times_table.c

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -1,24 +1,12 @@
 #include "main.h"
 
 /**
- * jack_bauer - Check main
- * Description: Print every hour and minute
- * Return: nothing
+ * main - check the code
+ *
+ * Return: Always 0.
  */
-void jack_bauer(void)
+int main(void)
 {
-	int hour, minute;
-	/* function */
-	for (hour = 0; hour <= 23; hour++)
-	{
-		for (minute = 0; minute <= 59; minute++)
-		{
-			_putchar((hour / 10) + '0');
-			_putchar((hour % 10) + '0');
-			_putchar(':');
-			_putchar((minute / 10) + '0');
-			_putchar((minute % 10) + '0');
-			_putchar('\n');
-		}
-	}
+    times_table();
+    return (0);
 }
