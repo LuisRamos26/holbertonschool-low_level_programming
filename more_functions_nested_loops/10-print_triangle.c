8b37247adfe104522844e3bd6d3e229191fbaad8
LuisRamos26 <angelramos261098@gmail.com>
2023-10-29 16:43:06 -0700

    triangle

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_diagonal -  draws a diagonal line on the terminal..
+ * @n: is the quantity of diagonals
+ * Return: .
+ */
+
+
+void print_diagonal(int n)
+{
+	int i;
+	int j;
+	/* Print diagonal.*/
+	if (n > 0)
+	{
+		for (i = 0; i < n ; i++)
+		{
+			for (j = 0; j < i; j++)
+			{
+				_putchar(' ');
+			}
+			_putchar('#');
+			_putchar('\n');
+		}
+	}
+	else
+		_putchar('\n');
+}
