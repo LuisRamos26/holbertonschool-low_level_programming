a94f9c1691f54ab5a36f430b7f43fd566f9f5466
LuisRamos26 <146479847+LuisRamos26@users.noreply.github.com>
2023-10-29 15:40:30 -0700

    Update 7-print_diagonal.c

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -13,17 +13,17 @@ void print_diagonal(int n)
 	int j;
 	/* Print diagonal.*/
 	if (n > 0)
-	{
-		for (i = 0; i < n ; i++)
-			{
-				for (j = 0; j < i; j++)
+		{
+			for (i = 0; i < n ; i++)
 				{
-					_putchar(' ');
+					for (j = 0; j < i; j++)
+					{
+						_putchar(' ');
+					}
+					_putchar(92);
+					_putchar('\n');
 				}
-				_putchar(92);
-				_putchar('\n');
-			}
-	}
-	else
-		_putchar('\n');
+		}
+		else
+			_putchar('\n');
 }
