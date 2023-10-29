640d5b4bb44a657619fba766edfcca45fe8a4f6a
LuisRamos26 <146479847+LuisRamos26@users.noreply.github.com>
2023-10-29 14:08:02 -0700

    Update 4-print_most_numbers.c

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -11,11 +11,11 @@ void print_most_numbers(void)
 	int i;
 	/* Print numbers.*/
 	for (i = 48; i <= 57; i++)
-	{	if (i != 50)
+	{	if (i == 50)
 		{ 
 		}
 		else if (i != 52)
-		_putchar(i);
+			_putchar(i);
 	}
 	_putchar('\n');
 
