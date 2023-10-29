cce87790f89b2021a1e49321d966dfd35436133e
LuisRamos26 <angelramos261098@gmail.com>
2023-10-29 14:03:13 -0700

    print numbers

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_numbers - Print numbers from 0 to 9.
+ * Return: .
+ */
+
+
+void print_numbers(void)
+{
+	int i;
+	/* Print numbers.*/
+	for (i = 48; i <= 57; i++)
+	{	if (i != 50)
+		else if (i != 52)
+		_putchar(i);
+	}
+	_putchar('\n');
+
+}
