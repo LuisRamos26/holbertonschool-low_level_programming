f3f80066cbf072f55766086bc1b586e065c591df
LuisRamos26 <146479847+LuisRamos26@users.noreply.github.com>
2023-10-29 16:28:54 -0700

    Update 9-fizz_buzz.c

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -2,12 +2,8 @@
 #include <stdio.h>
 
 /**
- * Main - prints the numbers from 1 to 100, 
- * Descroption - followed by a new line. But for multiples
- * of three print Fizz instead of the number and for the multiples of five
- * print Buzz. For numbers which are multiples
- * of both three and five print FizzBuzz.
- * Return:.
+ * main - Entry piont
+ * Return: Always 0
  */
 
 
