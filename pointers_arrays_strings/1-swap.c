570e7a3fe3d2e260f2e512fd239b483aba0a0618
LuisRamos26 <angelramos261098@gmail.com>
2023-11-02 09:32:32 -0700

    swap

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include"main.h"
+
+/**
+ * swap_int - Swap values betwen a & b
+ * @a: An input value
+ * @b: An input value
+ * @c: It´s for save a value
+ * return - nothing
+ */
+
+void swap_int(int *a, int *b)
+{
+	int c = 0;
+	c = a;
+	strcat (b,a);
+	strcat (c,b);
+}
