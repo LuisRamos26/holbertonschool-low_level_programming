7684ac403d3fdf73908ae587f479bc9cd7d9bbeb
LuisRamos26 <146479847+LuisRamos26@users.noreply.github.com>
2023-11-03 10:36:39 -0700

    Update 6-puts2.c

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -9,11 +9,13 @@
 void puts2(char *str)
 {
 	int len = 0;
+	int i;
 	/* count length of char */
 	while (str[len] != '\0')
+		len++;
+	for (i = 0; i < len; i = i + 2)
 	{
-		_putchar(str[len]);
-		len = len + 2;
+		_putchar(str[i]);
 	}
 	_putchar('\n');
 }
