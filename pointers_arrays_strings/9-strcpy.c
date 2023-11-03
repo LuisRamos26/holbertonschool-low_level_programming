c2c3eb67f416e02db091d4aec626bc5660f9565d
LuisRamos26 <angelramos261098@gmail.com>
2023-11-03 14:31:02 -0700

    strcpy

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,22 @@
+#include"main.h"
+#include <stdio.h>
+
+/**
+ * _strcpy -  prints n elements of an array of integers.
+ * @dest: A char input
+ * @src: A char input
+ * Return: dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+
+	while
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", a[i]);
+		if (i < n - 1)
+			printf(", ");
+	}
+	putchar('\n');
+}
