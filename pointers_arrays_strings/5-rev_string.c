c4160b29b9065f9ade8540040f3987b6f80a1c39
LuisRamos26 <146479847+LuisRamos26@users.noreply.github.com>
2023-11-03 07:47:15 -0700

    Update 5-rev_string.c

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -13,6 +13,8 @@ void rev_string(char *s)
 	/* count length of char */
 	while (s[len] != '\0')
 		len++;
+	for (i = 0; i >= (len -1); i++)
+		_putchar(s[i]);
 	for (i = (len - 1); i >= 0; i--)
 		_putchar(s[i]);
 	_putchar('\n');
