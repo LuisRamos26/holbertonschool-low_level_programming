0b4c65ab37eb2c48d8119476c3625e7f71430702
LuisRamos26 <146479847+LuisRamos26@users.noreply.github.com>
2023-11-03 11:37:09 -0700

    Update 8-print_array.c

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -11,10 +11,12 @@
 void print_array(int *a, int n)
 {
 	int i;
-	/* count length of char */
-	for (i = 0; i <= n; i++)
+
+	for (i = 0; i < n; i++)
 	{
-		
+		printf("%d", a[i]);
+		if (i < n - 1)
+			printf(", ");
 	}
-	_putchar('\n');
+	putchar('\n');
 }
