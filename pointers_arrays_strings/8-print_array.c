97d90e877755b55dc7f81d6017c229bf83a88d02
LuisRamos26 <angelramos261098@gmail.com>
2023-11-03 11:36:24 -0700

    print array

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,20 @@
+#include"main.h"
+#include <stdio.h>
+
+/**
+ * print_array -  prints n elements of an array of integers.
+ * @a: An input value
+ * @n: An input value 
+ * Return: Nothing
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+	/* count length of char */
+	for (i = 0; i <= n; i++)
+	{
+		
+	}
+	_putchar('\n');
+}
