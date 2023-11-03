2e651b212477dee7243cf33cb8b97f3834a72593
LuisRamos26 <146479847+LuisRamos26@users.noreply.github.com>
2023-11-03 07:37:52 -0700

    Update 4-print_rev.c

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -11,9 +11,9 @@ void print_rev(char *s)
 	int len = 0;
 	int i;
 	/* count length of char */
-	while (str[len] != '\0')
+	while (s[len] != '\0')
 		len++;
 	for (i = len; i >= 0; i--)
-		_putchar(s[i];)
+		_putchar(s[i]);
 	_putchar('\n');
 }
