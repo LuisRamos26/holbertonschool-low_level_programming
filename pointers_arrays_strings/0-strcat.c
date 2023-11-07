83b2355994be9d746afd622cb1f36d4e21f3afbb
LuisRamos26 <146479847+LuisRamos26@users.noreply.github.com>
2023-11-07 08:17:53 -0800

    Update 0-strcat.c

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -12,16 +12,16 @@ char *_strcat(char *dest, char *src)
 	int len1 = 0;
 	int len2 = 0;
 	/* count length of char */
-	while (src[len] != '\0')
+	while (src[len1] != '\0')
 	{
-		_putchar(src[len]);
-		len++;
+		_putchar(src[len1]);
+		len1++;
 	}
 	while (dest[len2] != '\0')
 	{
 		_putchar(dest[len2] != '\0');
 		len2++;
 	}	
-	dest = src[len] + dest[len2];
+	dest = src[len1] + dest[len2];
 	return(dest);
 }
