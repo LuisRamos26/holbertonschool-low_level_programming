5f61cdc71a3ba958ca897eb53192a1b399c91e44
LuisRamos26 <angelramos261098@gmail.com>
2023-11-08 10:42:14 -0800

    strncat

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,21 @@
+#include"main.h"
+
+/**
+ * *_strcat - Concatenate strings
+ * @src: An input string
+ * @dest: An input string
+ * Return: Dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	char *temp = dest;
+	int i;
+
+	while (*dest)
+		dest++;
+
+	for (i = 0; i <= n; i++)
+		*dest++ = *src++;
+	return (temp);
+}
