22a28e9470c8904d2c117111a2e86447e7a18d9d
LuisRamos26 <angelramos261098@gmail.com>
2023-11-08 11:31:17 -0800

    copies string

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,18 @@
+#include"main.h"
+
+/**
+ * *_strncpy - Concatenate strings
+ * @src: An input string
+ * @dest: An input string
+ * @n: An input value
+ * Return: temp
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	char *temp = dest;
+
+	while (*src)
+		*dest++ = *src++;
+	return (temp);
+}
