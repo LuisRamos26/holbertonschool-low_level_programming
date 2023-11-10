2b9809c327e7f13db090226df27f44463448b894
LuisRamos26 <146479847+LuisRamos26@users.noreply.github.com>
2023-11-10 08:13:31 -0800

    Update 2-strncpy.c

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -11,23 +11,25 @@
 char *_strncpy(char *dest, char *src, int n)
 {
 	int srclen = 0;
-	int i;
+	i = 0;
 	char *temp = dest;
-	char *start = src;
+	*start = src;
 
 	while (*src)
 	{
-		srclen++; /*Conteo para la longitud de src*/
+		srclen++;
 		src++;
 	}
+
 	srclen++;
+
 	if (n > srclen)
-		/*Sí el conteo de src es mayor que n, entonces n cambia su valor*/
 		n = srclen;
 
-	src = start; /*Vuelve el apuntador al incio de src*/
+	src = start;
 
 	for (i = 0; i < n; i++)
-		*dest++ = *src++; /*Copy src to dest*/
+		*dest++ = *src++;
+
 	return (temp);
 }
