907d3ccba18c9793202def32737656acdc72702a
LuisRamos26 <146479847+LuisRamos26@users.noreply.github.com>
2023-11-10 08:04:14 -0800

    Update 1-strncat.c

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -10,14 +10,14 @@
 
 char *_strncat(char *dest, char *src, int n)
 {
-	int srclen = 0
+	int srclen = 0;
 	int i;
 	char *temp = dest;
 	char *start = src;
 
 	while (*src)
 	{
-		srclen++; //Conteo para la longitud de src
+		srclen++; /*Conteo para la longitud de src*/
 		src++;
 	}
 
@@ -25,12 +25,12 @@ char *_strncat(char *dest, char *src, int n)
 		dest++;
 
 	if (n > srclen)
-		n = srclen; //Sí el conteo de src es mayor que n, entonces n cambia su valor
+		n = srclen; /*Sí el conteo de src es mayor que n, entonces n cambia su valor*/
 
-	src = start; //Vuelve el apuntador al incio de src
+	src = start; /*Vuelve el apuntador al incio de src*/
 
 	for (i = 0; i < n; i++)
-		*dest++ = *src++; //Concatena src a dest
+		*dest++ = *src++; /*Concatena src a dest*/
 
 	*dest = '\0';
 	return (temp);
