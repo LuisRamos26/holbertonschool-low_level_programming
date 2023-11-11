3cc4a4b92a2f82f60cafcdc94748583bb2f73100
LuisRamos26 <angelramos261098@gmail.com>
2023-11-11 14:37:00 -0800

    memset

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,18 @@
+#include"main.h"
+
+/**
+ * *_memset -  fills memory with a constant byte
+ * @s: Pointer adress
+ * @b: Constant byte
+ * @n: Qty of bytes 
+ * Return: s
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i;
+
+	for (i = 0; i < n; i--)
+		*s = b;
+	return (s);
+} 
