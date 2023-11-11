3ecd6bb698463776065a412cdf149191b1a996ef
LuisRamos26 <angelramos261098@gmail.com>
2023-11-11 10:20:50 -0800

    rev array

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,35 @@
+#include"main.h"
+
+/**
+ * reverse_array - Reverse a strings
+ * @a: An input value
+ * @n: An input value
+ * Return: temp
+ */
+
+void reverse_array(int *a, int n)
+{
+	int alen = 0;
+	int i = 0;
+	char *temp = a;
+	char *start = a;
+
+	while (*a)
+	{
+		alen++;
+		a++;
+	}
+
+	if (n > alen)
+		n = srclen;
+
+	for (i = 0; i < n; i++)
+	{
+		*start = *a;
+		start++;
+		a--;
+
+	}
+
+	return (temp);
+}
