9c0e392c316128ba5e85a2f5b2cc5472fa80ab57
LuisRamos26 <146479847+LuisRamos26@users.noreply.github.com>
2023-11-11 15:03:22 -0800

    Update 1-memcpy.c

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -13,13 +13,13 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 	unsigned int i;
 	char *start = dest;
 
-	while (n--)
+	for (i = 0; i < n; i++)
 	{
 		*dest = *src;
 		dest++;
 		src++;
 	}
-
 	return (start);
+	;
 }  
 
