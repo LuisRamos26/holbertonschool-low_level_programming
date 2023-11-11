3a00f2412346422ea2336b8fd72f5d37bbb93989
LuisRamos26 <angelramos261098@gmail.com>
2023-11-11 10:52:02 -0800

    string to upper

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include"main.h"
+
+/**
+ * *string_toupper -Change lowercase to uppercase
+ * @a: An input value
+ * Return: 0
+ */
+
+char *string_toupper(char *)
+{
+	int i = 0;
+
+	while (i < n)
+	{
+		n--;
+		aux = a[i];
+		a[i++] = a[n];
+		a[n] = aux;
+	}
+
+}
