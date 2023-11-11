58d6b1b0b46dbedd69df8c18325db63525819d79
LuisRamos26 <146479847+LuisRamos26@users.noreply.github.com>
2023-11-11 15:30:03 -0800

    Update 2-strchr.c

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -10,12 +10,14 @@
 
 char *_strchr(char *s, char c)
 {
+
 	while (*s)
 	{
-		if (*s == c)
+		if (c == *s)
 			return (s);
 		s++;
 	}
-	if (*s == c)
-		return (NULL);
+	if (c == *s)
+		return (s);
+	return (NULL);
 }
