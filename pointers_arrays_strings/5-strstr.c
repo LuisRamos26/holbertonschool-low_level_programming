eb32edaee1003e9919c00308755b84bed873a01a
LuisRamos26 <angelramos261098@gmail.com>
2023-11-12 08:57:18 -0800

    strsta

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,31 @@
+#include"main.h"
+#include<stdio.h>
+
+/**
+ * *_strstr - locates a substring.
+ * @haystack: An input string
+ * @needle: An input string to find it.
+ * Return: a pointer to the beginning of the located substrin
+ * or NULL if the substring is not found
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	char *starth = haystack;
+	char *startn = needle;
+
+	while (*haystack)
+	{
+		starth = haystack;
+		needle = startn;
+		while (*haystack == *needle)
+		{
+			haystack++;
+			needle++;
+		}
+		if (*needle == '\0')
+			return(haystack);
+		haystack = starth +1;
+	}
+	return(NULL);
+}
