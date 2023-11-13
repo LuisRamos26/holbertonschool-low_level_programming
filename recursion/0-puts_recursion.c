65fb3c25ebb2f625791c272f06d5f747aaa67ddc
LuisRamos26 <146479847+LuisRamos26@users.noreply.github.com>
2023-11-12 17:25:39 -0800

    Update 0-puts_recursion.c

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -2,14 +2,14 @@
 
 /**
 * _puts_recursion - prints a string, followed by a new line.
-* @s: An input string 
+* @s: An input string
 *return: Nothing
 */
 
 void _puts_recursion(char *s)
 {
-	if(*s)
-	{	
+	if (*s)
+	{
 		_putchar(*s);
 		_puts_recursion(s + 1);
 	}
