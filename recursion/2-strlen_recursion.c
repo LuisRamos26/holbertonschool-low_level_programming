94aa855088190c9678ce4021abe3765dbbc91e80
LuisRamos26 <angelramos261098@gmail.com>
2023-11-12 18:06:10 -0800

    strel recursion

---
+++ recursion/2-strlen_recursion.c
@@ -0,0 +1,18 @@
+#include"main.h"
+
+/**
+* _strlen_recursion - returns the length of a string.
+* @s: An input string
+*return: Length of a string.
+*/
+
+int _strlen_recursion(char *s)
+{
+	slen = 0;
+
+	if (*s)
+	{
+		_strlen_recursion(s + 1);
+		slen++;
+	}
+}
--- recursion/main.h
+++ recursion/main.h
@@ -2,3 +2,5 @@ void _puts_recursion(char *s);
 int main(void);
 int _putchar(char c);
 void _print_rev_recursion(char *s);
+void _print_rev_recursion(char *s);
+int _strlen_recursion(char *s);
