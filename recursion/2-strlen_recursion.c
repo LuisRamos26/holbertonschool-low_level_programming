9b74e4e2c223a98e88cd14a3abce2eca8b304ef8
LuisRamos26 <146479847+LuisRamos26@users.noreply.github.com>
2023-11-12 18:10:31 -0800

    Update 2-strlen_recursion.c

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -1,7 +1,7 @@
 #include"main.h"
 
 /**
-* _strlen_recursion - returns the length of a string.
+* _strlen_recursion- returns the length of a string.
 * @s: An input string
 *return: Length of a string.
 */
