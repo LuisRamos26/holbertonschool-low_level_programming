f2c8fa850025148eb3e6c01d2914d7254822b81f
LuisRamos26 <146479847+LuisRamos26@users.noreply.github.com>
2023-11-19 17:14:43 -0800

    Add files via upload

---
+++ static_libraries/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs - check main.h
+ * @value: an integer input
+ * Description: returns absolute value of a number
+ * Return: Absolute value
+ */
+int _abs(int value)
+{
+	if (value >= 0)
+		return (value);
+	else
+		return (value * -1);
+}
