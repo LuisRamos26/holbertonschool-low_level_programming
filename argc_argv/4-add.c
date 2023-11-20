efe563239c25f2464e6f774f8b8894690c200d0c
LuisRamos26 <146479847+LuisRamos26@users.noreply.github.com>
2023-11-19 21:13:14 -0800

    Update 4-add.c

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -1,16 +1,30 @@
-#include "main.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
 
 /**
- * main - a program that prints its name, followed by a new line
- * @argc: arguement count
- * @argv: argument value, a strings that come after calling function
+ * main - A program that adds positive numbers
+ * @argc: The arguments' counter
+ * @argv: The argument's values
  * Return: Always 0 (Success)
  */
-
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
-	if (argv[0])
-		printf("%d\n", argc - 1);
+	int num, result = 0, i;
+
+	while (argc-- > 1)
+	{
+		for (i = 0; argv[argc][i]; i++)
+		{
+			if (!(isdigit(argv[argc][i])))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		num = atoi(argv[argc]);
+		result += num;
+	}
+	printf("%d\n", result);
 	return (0);
 }
