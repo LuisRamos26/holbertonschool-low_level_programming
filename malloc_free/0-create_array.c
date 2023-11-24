ba60d4c424817efe9132b416e869af893db6591b
LuisRamos26 <146479847+LuisRamos26@users.noreply.github.com>
2023-11-24 10:16:43 -0800

    Update 0-create_array.c

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -9,18 +9,21 @@
  * @size: the size of the memory to print
  * Return: A pointer to the array, or NULL if it fails.
  */
-
 char *create_array(unsigned int size, char c)
 {
-  char *point;
-  unsigned int i;
+	char *s;
+	unsigned int i = 0;
 
-  point = malloc(size * sizeof(char));
-  if (point == NULL)
-    return(NULL);
-  if (size == 0)
-    return(NULL);
-  for (i=0; i < size ;i++)
-    point[i] = c;
-  return(point);
+	s = malloc(size * sizeof(char));
+
+	if (s == NULL)
+		return (NULL);
+	if (size == 0)
+		return (NULL);
+	while (i < size)
+	{
+		s[i] = c;
+		i++;
+	}
+	return (s);
 }
