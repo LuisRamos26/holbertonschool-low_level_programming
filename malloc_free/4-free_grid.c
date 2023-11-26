0f7071145906363ca20787bf216fe4a1a21d02e7
LuisRamos26 <146479847+LuisRamos26@users.noreply.github.com>
2023-11-26 09:44:29 -0800

    Update 4-free_grid.c

--- malloc_free/4-free_grid.c
+++ malloc_free/4-free_grid.c
@@ -3,54 +3,19 @@
 #include <stdlib.h>
 
 /**
- * *str_concat - concatenates two strings.
- * @s1: input string
- * @s2: input string
- * Return: newly allocated space in memory which contains the
- * concatenated strings.
- * if NULL is passed, treat it as an empty string
- * The function should return NULL on failure
+ * free_grid - A function that frees a 2D array of ints previously
+ * created by your malloc_grid function
+ * @grid: An input 2D array of integers to free
+ * @height: height of grid
+ * Return: Nothing
  */
-char *str_concat(char *s1, char *s2)
+void free_grid(int **grid, int height)
 {
-	char *new_str, *starts1, *starts2;
-	int i = 0, lens1 = 0, lens2 = 0;
+	int i = 0;
 
-	starts1 = s1;
-	starts2 = s2;
-	if (s1 == NULL)
-		s1 = "";
-	while (*s1)
-	{
-		lens1++;
-		s1++;
-	}
-	s1 = starts1;
-	if (s2 == NULL)
-		s2 = "";
-	while (*s2)
-	{
-		lens2++;
-		s2++;
-	}
-	s2 = starts2;
-	new_str = malloc(sizeof(char) * (lens1 + lens2 + 1));
-	starts1 = new_str;
-	if (new_str == NULL)
-		return (NULL);
-	for (; i < (lens1 + lens2); i++)
-	{
-		if (i < lens1)
-		{
-			new_str[i] = *s1;
-			s1++;
-		}
-		else
-		{
-			new_str[i] = *s2;
-			s2++;
-		}
-	}
-	new_str[i] = '\0';
-	return (starts1);
+	if (grid == NULL)
+		return;
+	while (i < height)
+		free((int *)grid[i++]);
+	free(grid);
 }
