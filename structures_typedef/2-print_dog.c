bef9f9b62ea7f0a6be58a338c2e0d8a3f0dade78
LuisRamos26 <146479847+LuisRamos26@users.noreply.github.com>
2023-12-03 16:16:25 -0800

    Update 2-print_dog.c

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -1,22 +1,19 @@
-#ifndef _DOG_H_
-#define _DOG_H_
+#include "dog.h"
+#include <stdio.h>
 
 /**
- * struct dog - structure for a basic dog
- * @name: Dog's name
- * @age: Dog's age
- * @owner: Dog's owner
- * Description: data structure for dog
+ * print_dog - a function that prints a struct dog.
+ * @d: struct to initialize
+ * Return: Nothing.
  */
-struct dog
+void print_dog(struct dog *d)
 {
-	char *name;
-	float age;
-	char *owner;
-};
-typedef struct dog dog_t;
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
-#endif
+	if (d == NULL)
+		return;
+	if (d->name == NULL)
+		d->name = "(nil)";
+	if (d->owner == NULL)
+		d->owner = "(nil)";
+
+	printf("Name: %s\nAge: %0.6f\nOwner: %s\n", d->name, d->age, d->owner);
+}
