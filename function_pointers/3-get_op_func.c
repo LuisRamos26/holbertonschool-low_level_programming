377c61e2653d6bc27ec9bec06ec1a4f04ab898c1
LuisRamos26 <146479847+LuisRamos26@users.noreply.github.com>
2023-12-10 16:13:41 -0800

    Update 3-get_op_func.c

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -1,15 +1,30 @@
 #include <stdlib.h>
-#include "function_pointers.h"
+#include "3-calc.h"
 
 /**
- * print_name - a function that prints a name.
- * @name: pointer to name to print
- * @f: function to print name
- * Return: Nothing
+ * get_op_func -  the function that selects the correct function
+ * to perform the operation asked by the user.
+ * @s: An input char pointer operator passed as argument
+ * Return: a pointer to the function that corresponds to the
+ * operator given as a parameter.
  */
-void print_name(char *name, void (*f)(char *name))
+int (*get_op_func(char *s))(int, int)
 {
-	if (name == NULL || f == NULL)
-		return;
-	f(name);
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i = 0;
+
+	while (i < 5)
+	{
+		if (*(ops[i]).op == *s && *(s + 1) == '\0')
+			return (ops[i].f);
+		i++;
+	}
+	return (NULL);
 }
